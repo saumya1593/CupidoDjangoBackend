4d89dcfde5b026073bc1a8b83920c4e166f1ad2e
saumya1593 <p15.saumya@gmail.com>
2019-07-29 22:04:08 +0530

    Making Static

--- CupidoDjangoBackend/settings.py
+++ CupidoDjangoBackend/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
